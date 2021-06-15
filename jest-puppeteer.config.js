6d6dad95a8f05d2d296960d5dfc475c376472c8e
michael19 <rekmixa@gmail.com>
2021-06-15 23:18:08 +0500

    change Dockerfile

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
+
 module.exports = {
   launch: {
     dumpio: true,
     headless: process.env.HEADLESS === 'true',
+    executablePath: process.env.CHROME_PATH,
     product: 'chrome',
   },
   browserContext: 'default',
