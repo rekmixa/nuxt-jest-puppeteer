3c3be28a987467a4d6f28e3b04753b58afdb2641
michael19 <rekmixa@gmail.com>
2021-06-15 23:05:00 +0500

    install jest-puppeteer & add tests/hello-world.test.js

--- JEST_PUPPETEER.md
+++ JEST_PUPPETEER.md
@@ -1,5 +1,22 @@
 # Installation
 
+https://jestjs.io/ru/docs/puppeteer
+
+Node JS requirements:  
+`^10.13.0 || ^12.13.0 || ^14.15.0 || >=15.0.0`
+
 ```bash
-yarn add jest-puppeteer --dev
+yarn add puppeteer puppeteer-core jest-puppeteer --dev
+```
+
+Add to *jest.config.js*:  
+
+```js
+preset: 'jest-puppeteer',
+```
+
+## Run first test
+
+```bash
+yarn test tests/hello-world.test.js
 ```
--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,23 @@
 module.exports = {
+  preset: 'jest-puppeteer',
+
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js'
   },
+
   moduleFileExtensions: [
     'js',
     'vue',
     'json'
   ],
+
   transform: {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest'
   },
+
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
