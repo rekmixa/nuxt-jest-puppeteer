e5d1e9dd36864f43ce4343f46988e5b1fe060ce5
michael19 <rekmixa@gmail.com>
2021-06-23 22:32:54 +0500

    change jest-puppeteer configs

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 
+const noSandboxArgs = [
+  '--no-sandbox',
+  '--disable-setuid-sandbox'
+]
+
 module.exports = {
   launch: {
     dumpio: true,
     headless: process.env.HEADLESS === 'true',
     executablePath: process.env.CHROME_PATH,
+    args: process.env.NO_SANDBOX === 'true' ? noSandboxArgs : [],
     product: 'chrome',
   },
   browserContext: 'default',
